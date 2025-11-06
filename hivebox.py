f0af392d47bc1b105f51bee8128f3178e622dfad
dugget <twitt23@gmail.com>
2025-11-06 11:59:22 -0500

    feat: initial commit

---
+++ hivebox.py
@@ -0,0 +1,15 @@
+"""
+HiveBox API - A DevOps tooling API
+version 0.1.0
+"""
+
+__version__ = "0.1.0"
+
+def print_version():
+    """
+    Print the current version of the HiveBox API
+    """
+    print(f"HiveBox API version: {__version__}")
+
+if __name__ == "__main__":
+    print_version()
